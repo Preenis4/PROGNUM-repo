83eb369acbba07c0811b8b01889bf8aceeb2842b
Jinne <alberdajinne@gmail.com>
2026-03-30 14:05:38 +0200

    classes started

--- Task8/Galactic_Golf.py
+++ Task8/Galactic_Golf.py
@@ -1,18 +1,26 @@
 import pygame
+import sys
 import numpy as np
+from astropy.io import fits
 
 COLOR = (255, 100, 98)
+size = 10
 SURFACE_COLOR = (167, 255, 100)
 ScreenWideness = 500
 ScreenHighness = 500
 ShootAngle = 0
 FRICTION = 10
+power = 0
+setup = True
+data =
 
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self, color, highness, wideness, upness):
         super().__init__()
-        self.image = pygame.Surface([wideness*upness//10, highness*upness//10])
+        self.wideness = wideness
+        self.highness = highness
+        self.image = pygame.Surface([wideness, highness])
         self.image.fill(SURFACE_COLOR)
         self.image.set_colorkey(COLOR)
 
@@ -20,23 +28,45 @@ class Ball(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
 
-    def MoveRight(self, vel):
-        self.rect.x += vel
+    def move(self, (x, y), vel):
+        self.rect.x += x*vel
+        self.rect.y += y*vel
 
-    def MoveLeft(self, vel):
-        self.rect.x -= vel
+    def elevate(self, upness):
+        self.upness = upness
+        self.image = pygame.Surface([wideness*(1 + upness/100), highness*(1 + upness/10)])
 
-    def MoveUp(self, vel):
-        self.rect.y -= vel
+class Field:
+    def __init__(self, wideness, highness, data):
+        ysz, xsz = np.shape(data)
+        for j in data:
+            for i in j:
+                pass
 
-    def MoveDown(self, vel):
-        self.rect.y += vel
 
-def vector_move(sprite, (x, y), vel):
-    t
+myball = Ball(COLOR, size, size, 0)
+
+while True:
+    for event in pygame.event.get()
+        if event.type == pygame.QUIT:
+            sys.exit()
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_SPACE]:
+        setup = False
+
+    if setup:
+        if keys[pygame.K_UP]:
+            power += 1
+        if keys[pygame.K_DOWN]:
+            power -= 1
+        if keys[pygame.K_RIGHT]:
+            ShootAngle -= 1
+        if keys[pygame.K_LEFT]:
+            ShootAngle += 1
+    else:
 
 pygame.init()
 screen = pygame.display.set_mode((ScreenWideness, ScreenHighness))
 klok = pygame.time.Clock()
-
 pygame.display.set_caption("Galactic Golf DX")
---
+++ Task8/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+z = np.zeros(10)
+z = np.reshape(z, (5, 2))
+print(z)
+print(np.size(z))
